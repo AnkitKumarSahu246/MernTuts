2b9854b67098108401b4ca176455bd68dd6a76c4
AnkitKumarSahu246 <itzmeankitkumarsahu2000246@gmail.com>
2020-07-10 21:53:04 +0530

    post method for delete and add

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express'); //importing 3rd party module
+const bodyParser = require('body-parser');
 const app = express() //invoking express
 const port = 5000;
 
 //Serving static files
 app.use(express.static('public'));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+
+//parse application/json
+app.use(bodyParser.json());
 
 //Setting view engine
 app.set('view engine','ejs');
--- routes.js
+++ routes.js
@@ -1,10 +1,7 @@
 const express = require('express'); //importing 3rd party module
 const app = express() //invoking express
 
-data = {
-    name:"ankit",
-    status:"programmer"
-}
+var data  = ['C','C++','Python','JavaScript'];    
 
 module.exports = (app)=>{
     app.get('/',(req,res)=>{
@@ -13,5 +10,22 @@ module.exports = (app)=>{
     
     app.get('/about',(req,res)=>{
         res.render('about');
+    });
+
+    app.post('/sent',(req,res)=>{
+        console.log(req.body.langs);
+        data.push(req.body.langs);
+        res.send(data);
+    });
+
+    app.delete('/remove/:id',(req,res)=>{
+
+        data = data.map(langs=>{
+            if(langs != req.params.id){
+                return langs;
+            }
+        });
+        console.log(req.params.id);
+        res.send(data);
     });
 }
